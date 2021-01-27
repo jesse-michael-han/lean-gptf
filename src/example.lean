05638167934a98d4c5b634287e432bd201b8416e
Jesse Michael Han <hyrodi@gmail.com>
2021-01-27 12:58:14 -0500

    feat(src/example): update examples with new engine

--- src/example.lean
+++ src/example.lean
@@ -1,4 +1,5 @@
 import tactic.gptf
+import data.list.sigma
 
 example {α} (a : α) : a = a :=
 begin
@@ -7,12 +8,12 @@ end
 
 example : ∃ n : ℕ, 8 = 2*n :=
 begin
-  exact ⟨4, by norm_num⟩
+  exact dec_trivial
 end
 
 example {P Q R : Prop} : P → (P → R) → R :=
 begin
-  tauto
+  intro h1, exact λ h2, h2 h1
 end
 
 example {p q r : Prop} (h₁ : p) (h₂ : q) : (p ∧ q) ∨ r :=
@@ -22,15 +23,38 @@ end
 
 example {P Q : Prop} : (¬ P) ∧ (¬ Q) → ¬ (P ∨ Q) :=
 begin
-  rintro ⟨hp, hq⟩, rw not_or_distrib, exact ⟨hp, hq⟩
+  rintro ⟨h₁, h₂⟩, simp [h₁, h₂]
 end
 
 example {P Q R : Prop} : (P ∧ Q) → ((P → R) → ¬ (Q → ¬ R)) :=
 begin
-  rintros ⟨h₁, h₂⟩ h₃, simp [@h₂], exact h₃ h₁
+  rintro ⟨h₁, h₂⟩ h₃, apply not_imp_of_and_not, apply and.intro h₂,
+  rw [not_not], apply h₃ h₁
 end
 
 example (n : ℕ) (m : ℕ) : nat.succ n < nat.succ n + 1  :=
 begin
   apply nat.lt_succ_self
+end
+
+example : ∀ (F1 F2 F3 : Prop), ((¬F1 ∧ F3) ∨ (F2 ∧ ¬F3)) → (F2 → F1) → (F2 → F3) →  ¬F2 :=
+begin
+  tauto
+end
+
+example : ∀ (f : nat → Prop), f 2 → ∃ x, f x :=
+begin
+  exact λ f hf, Exists.intro 2 hf
+end
+
+example {G} [group G] (x y z : G) : (x * z) * (z⁻¹ * y) = x * y :=
+begin
+  simp [mul_assoc]
+end
+universes u v
+
+example {α : Type u} {β : α → Type v} [_inst_1 : decidable_eq α] {a : α} {l₁ l₂ : list (sigma β)} :
+  (list.kerase a l₁).kunion (list.kerase a l₂) = list.kerase a (l₁.kunion l₂) :=
+begin
+  induction l₁; simp
 end
